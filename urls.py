6f37900535f8c1c9dc5dfe89d8d1fa9fd48e564a
Adnan Majeed <49750395+adnanmajeed82@users.noreply.github.com>
2026-04-14 14:07:18 +0500

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,9 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProgramViewSet, StudentViewSet
 
-from django.contrib import admin
-from django.urls import path,include
+
+# Create router
+router = DefaultRouter()
+router.register(r'programs', ProgramViewSet, basename='program')
+router.register(r'students', StudentViewSet, basename='student')
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/',include('app.urls'))
-
+    path('', include(router.urls)),
 ]
